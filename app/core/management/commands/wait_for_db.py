5fec1db1261454a983cff6fdc7ac75e3156764a9
ANILKE <anilkekevi.44@gmail.com>
2024-03-04 17:29:02 +0300

    Basic User API and its unit tests are implemented.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,7 +6,6 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
-
 class Command(BaseCommand):
     """Django Command for wait for the database."""
     def handle(self, *args, **options):
@@ -15,9 +14,9 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                self.check(databases = ['default'])
                 db_up = True
             except(Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write('Database unavailable, waiting for 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database Available!'))
